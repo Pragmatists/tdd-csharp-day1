ab0cc056648c81425cf292bba4ca8ad3918c3359
Krzysztof Jelski <Krzysztof Jelski>
2016-03-17 09:47:40 +0100

    Add FizzBuzz starter

---
+++ FizzBuzzKata/FizzBuzzTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void Returns1For1()
+        {
+            Assert.AreEqual(2, 4);
+        }
+    }
+}
